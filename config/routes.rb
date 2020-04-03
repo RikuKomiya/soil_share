27a4239e14371d7a602ef0ae819ba170cac36d68
Riku Komiya <824rikukomiya@gmail.com>
2020-04-03 15:09:43 +0900

    修正漏れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'home#index'
   devise_for :users
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
